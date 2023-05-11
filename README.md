41c38c3271dd61da31443f0bdb78ad64b18babd0
RonPhysics <ron.cohen1@biu.ac.il>
2023-05-11 13:27:35 +0300

    Revised documentation: Added examples in documentation and main().

--- README.md
+++ README.md
@@ -13,23 +13,38 @@ Authors: Yuval Grinberg, Ron Cohen, Benjamin Ramati
 - main: run examples
 
 ### How to use the code?
-Create the respective data structures
+Create the respective data structures:
+Here we create an extended field for prime field (p=7) by using a second order irreducible polynomial
+with coefficients (3,6,1)
 ```python
 p = 7
-irreducible_polynom = [3, 6, 1]
+irreducible_polynom = [3, 6, 1] #It must be irreducible for prime field members (p=7)
 finite_field = FiniteField(p, irreducible_polynom)
 ```
+Creating the element (4,5) from the field
+````python
+finite_field_element = FiniteFieldElement(finite_field, [4, 5])
+print(f"You have created the element {finite_field_element}")
+````
 
-Find a generator element in the field
+Find the order of an element (for the multiplicative group, l-{0}) 
+````python
+my_element_order = finite_field_element.get_multiplicative_order()
+print(f"The order of your chosen element is {my_element_order}")
+````
+
+Find a generator (for the multiplicative group, l-{0}) element in the field
 ```python
 generator_element = find_generator_for_field(finite_field)
+print(f"The generator that was found is: {generator_element}")
 ```
 
-Run the baby step giant step algorithm to get the result
+Our element "finite_field_element" can be produced by exponentiating a generator to the appropriate power,
+such that for some integer value "t" exists  finite_field_element = generator_element^t.
+Suppose we wish to find the number "t" for which we can reach our element, to do so we may use the Baby Step Giant Step (BSGS) algorithm.
+Here is an example, using the element and generator we have.
 ```python
-t = 40 % finite_field.p
-exp_element = gen ** t
-bsgs = baby_step_giant_step(generator_element, exp_element)
-assert bsgs == t
+t = baby_step_giant_step(generator_element, finite_field_element)
+print(f"The exponent power t={t}, for which finite_field_element = generator_element^t")
 ```
 
--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 from algo.BSGS import baby_step_giant_step
 from algo.generator_creation import find_generator_for_field
 from models.finite_field import FiniteField
+from models.finite_field_element import FiniteFieldElement, get_e0_element, get_e1_element
 
 
 def main():
     print("Authors: Benjamin, Ron, and Yuval")
 
-    # Create the respective data structures
+    # Create the respective data structures:
+    # Here we create an extended field for prime field (p=7) by using a second order irreducible polynomial
+    # with coefficients (3,6,1)
     p = 7
     irreducible_polynom = [3, 6, 1]
     finite_field = FiniteField(p, irreducible_polynom)
 
-    # Find a generator element in the field
-    generator_element = find_generator_for_field(finite_field)
+    # Creating the element (4,5) from the field
+    finite_field_element = FiniteFieldElement(finite_field, [4, 5])
+    print(f"You have created the element {finite_field_element}")
 
-    # Run the baby step giant step algorithm to get the result
-    t = 40 % finite_field.p
-    exp_element = generator_element ** t
-    bsgs = baby_step_giant_step(generator_element, exp_element)
-    print(f"BSGS result: {bsgs}")
-    assert bsgs == t
+    # Find the order of an element (for the multiplicative group, l-{0})
+    my_element_order = finite_field_element.get_multiplicative_order()
+    print(f"The order of your chosen element is {my_element_order}")
+
+    # Find a generator (for the multiplicative group, l-{0}) element in the field
+    generator_element = find_generator_for_field(finite_field)
+    print(f"The generator that was found is: {generator_element}")
+
+    # BSGS:
+    # Our element "finite_field_element" can be produced by exponentiating a generator to the appropriate power,
+    # such that for some integer value "t" exists  finite_field_element = generator_element^t.
+    # Suppose we wish to find the number "t" for which we can reach our element, to do so we may use the Baby Step Giant Step (BSGS) algorithm.
+    # Here is an example, using the element and generator we have.
+
+    t = baby_step_giant_step(generator_element, finite_field_element)
+    print(f"The exponent power t={t}, for which finite_field_element = generator_element^t")
+
+
 
 
 if __name__ == "__main__":
